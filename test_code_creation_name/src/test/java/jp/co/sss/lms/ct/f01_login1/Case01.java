ad8f229e1d18c4af457d4806d0b19b5aa4c6e182
r-fujii <r_fujii@braingate.co.jp>
2026-03-24 10:42:20 +0900

    add:Case01.java ログイン画面への遷移
    WebElementを利用してタイトルの値とタグ値を取得し、それぞれ期待値通りかどうか確認するテストを作成した。
    期待値は以下の通り
    タイトル：ログイン
    タグ：ログイン
    
    add:Case02.java ログイン画面への遷移、エラーメッセージ確認
    WebElementを利用してDB未登録のID/PWを入力し、期待値通りに動作するか確認するテストを作成した。
    期待値は以下の通り
    エラーメッセージ：ログインに失敗しました。
    
    add:docファイルの追加
    仕様書の保存
    
    add:evidenceファイルの追加
    エビデンスの保存

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,7 +37,19 @@ public class Case01 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		// URLにアクセス
+		goTo("http://localhost:8080/lms");
+
+		pageLoadTimeout(10);
+
+		// 検証：トップページ
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		assertEquals("ログイン", webDriver.findElement(By.tagName("h2")).getText());
+
+		// エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
