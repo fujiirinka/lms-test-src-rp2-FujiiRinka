3660996422e10ad87227b4942f0d9aa7dc541b4e
r-fujii <r_fujii@braingate.co.jp>
2026-03-27 13:28:18 +0900

    add:Case07.java 受講生レポート新規登録(日報)正常系
    テストコード、エビデンスの追加
    日報未提出の研修日を選択し、報告内容を入力し、提出。
    提出後、ボタン名が更新されていることを確認。
    
    add:Case08.java 受講生レポート修正(週報)正常系
    テストコード、エビデンスの追加
    提出済みの週報の内容を修正し、提出。
    上部メニューのユーザーリンクからユーザー詳細画面へ遷移し、週報が更新されていることを確認。
    
    fix:結合テスト仕様書 No.07,08
    テスト内容の追記

--- test_code_creation_fujiirinka/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
+++ test_code_creation_fujiirinka/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト レポート機能
@@ -35,35 +40,99 @@ public class Case07 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		// URLにアクセス
+		goTo("http://localhost:8080/lms");
+
+		// 検証：トップページ
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		assertEquals("ログイン", webDriver.findElement(By.tagName("h2")).getText());
+
+		// エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		// 初回ログイン済みのID/PWを入力、クリック
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA02");
+		webDriver.findElement(By.id("password")).sendKeys("StudentAA02");
+		webDriver.findElement(By.className("btn")).click();
+
+		pageLoadTimeout(10);
+
+		// 検証：コース詳細画面への遷移
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		// エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 未提出の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		scrollTo("200");
+
+		// 「詳細」ボタンをクリック
+		List<WebElement> submitButtonList = webDriver.findElements(By.cssSelector("input[type = 'submit']"));
+		submitButtonList.get(3).click();
+
+		// 検証：セクション詳細画面への遷移
+		assertEquals("関係演算子、条件分岐、繰り返し 2022年10月6日", webDriver.findElement(By.tagName("h2")).getText());
+		assertEquals("セクション詳細 | LMS", webDriver.getTitle());
+
+		// エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「提出する」ボタンを押下しレポート登録画面に遷移")
 	void test04() {
-		// TODO ここに追加
+
+		// 「日報【デモ】を提出する」ボタンをクリック
+		WebElement submitBtn = webDriver.findElement(By.cssSelector("input[type = 'submit']"));
+		submitBtn.click();
+
+		// 検証：レポート登録画面への遷移
+		assertEquals("日報【デモ】 2022年10月6日", webDriver.findElement(By.tagName("h2")).getText());
+		assertEquals("レポート登録 | LMS", webDriver.getTitle());
+
+		// エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 報告内容を入力して「提出する」ボタンを押下し確認ボタン名が更新される")
 	void test05() {
-		// TODO ここに追加
+
+		// 報告内容の入力、クリック
+		WebElement reportForm = webDriver.findElement(By.className("form-control"));
+		String report = "日報提出テストです。";
+		WebElement submitBtn = webDriver.findElement(By.cssSelector("button[type = 'submit']"));
+
+		reportForm.clear();
+		reportForm.sendKeys(report);
+		submitBtn.click();
+
+		// 検証：確認ボタン名の更新を確認
+		WebElement checkBtn = webDriver.findElement(By.cssSelector("input[type = 'submit']"));
+		assertEquals("提出済み日報【デモ】を確認する", checkBtn.getAttribute("value"));
+		assertEquals("セクション詳細 | LMS", webDriver.getTitle());
+
+		// エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
--- test_code_creation_fujiirinka/src/test/java/jp/co/sss/lms/ct/f03_report/Case08.java
+++ test_code_creation_fujiirinka/src/test/java/jp/co/sss/lms/ct/f03_report/Case08.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト レポート機能
@@ -35,49 +40,161 @@ public class Case08 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+
+		// URLにアクセス
+		goTo("http://localhost:8080/lms");
+
+		// 検証：トップページ
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		assertEquals("ログイン", webDriver.findElement(By.tagName("h2")).getText());
+
+		// エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+
+		// 初回ログイン済みのID/PWを入力、クリック
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA02");
+		webDriver.findElement(By.id("password")).sendKeys("StudentAA02");
+		webDriver.findElement(By.className("btn")).click();
+
+		pageLoadTimeout(10);
+
+		// 検証：コース詳細画面への遷移
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+
+		// エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 提出済の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+
+		// 「詳細」ボタンをクリック
+		List<WebElement> submitButtonList = webDriver.findElements(By.cssSelector("input[type = 'submit']"));
+		submitButtonList.get(1).click();
+
+		// 検証：セクション詳細画面への遷移
+		assertEquals("アルゴリズム、フローチャート 2022年10月2日", webDriver.findElement(By.tagName("h2")).getText());
+		assertEquals("セクション詳細 | LMS", webDriver.getTitle());
+
+		// エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「確認する」ボタンを押下しレポート登録画面に遷移")
 	void test04() {
-		// TODO ここに追加
+
+		scrollTo("500");
+
+		// 「提出済み週報【デモ】を確認する」ボタンをクリック
+		WebElement checkBtn = webDriver.findElement(By.cssSelector("input[value = '提出済み週報【デモ】を確認する']"));
+		checkBtn.click();
+
+		// 検証：レポート登録画面への遷移
+		assertEquals("週報【デモ】 2022年10月2日", webDriver.findElement(By.tagName("h2")).getText());
+		assertEquals("レポート登録 | LMS", webDriver.getTitle());
+
+		// エビデンスの取得(1)
+		getEvidence(new Object() {
+		}, "upper");
+
+		scrollTo("500");
+
+		// エビデンスの取得(2):test05 ページ下部比較のため
+		getEvidence(new Object() {
+		}, "lower");
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 報告内容を修正して「提出する」ボタンを押下しセクション詳細画面に遷移")
 	void test05() {
-		// TODO ここに追加
+
+		// 報告内容の修正
+		WebElement impression = webDriver.findElement(By.id("content_1"));
+		String fixImpression = "週報修正テストです。";
+		WebElement lookBack = webDriver.findElement(By.id("content_2"));
+		String fixLookBack = "来週もよろしくお願いします。";
+
+		impression.clear();
+		impression.sendKeys(fixImpression);
+		lookBack.clear();
+		lookBack.sendKeys(fixLookBack);
+
+		// エビデンスの取得(1)
+		getEvidence(new Object() {
+		}, "fixReport");
+
+		// 「提出する」ボタンをクリック
+		WebElement submitBtn = webDriver.findElement(By.cssSelector("button[type = 'submit']"));
+		submitBtn.click();
+
+		pageLoadTimeout(10);
+
+		// 検証：セクション詳細画面への遷移
+		assertEquals("アルゴリズム、フローチャート 2022年10月2日", webDriver.findElement(By.tagName("h2")).getText());
+		assertEquals("セクション詳細 | LMS", webDriver.getTitle());
+
+		// エビデンスの取得(2):
+		getEvidence(new Object() {
+		}, "pageTransition");
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 上部メニューの「ようこそ○○さん」リンクからユーザー詳細画面に遷移")
 	void test06() {
-		// TODO ここに追加
+
+		// 「ようこそ受講生AA2さん」リンクをクリック
+		WebElement userLink = webDriver.findElement(By.linkText("ようこそ受講生ＡＡ２さん"));
+		userLink.click();
+
+		// 検証：ユーザー詳細画面への遷移
+		assertEquals("ユーザー詳細", webDriver.findElement(By.tagName("h2")).getText());
+		assertEquals("ユーザー詳細", webDriver.getTitle());
+
+		// エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(7)
 	@DisplayName("テスト07 該当レポートの「詳細」ボタンを押下しレポート詳細画面で修正内容が反映される")
 	void test07() {
-		// TODO ここに追加
+
+		scrollTo("500");
+
+		// 「詳細」ボタンをクリック
+		List<WebElement> submitButtonList = webDriver.findElements(By.cssSelector("input[value = '詳細']"));
+		submitButtonList.get(2).click();
+
+		// 検証：レポートの詳細画面への遷移
+		assertEquals("週報【デモ】 2022年10月2日", webDriver.findElement(By.tagName("h2")).getText());
+		assertEquals("レポート詳細 | LMS", webDriver.getTitle());
+
+		// 検証：報告内容更新の確認
+		WebElement updateImpression = webDriver.findElement(By.xpath("//td[contains(text(), '修正テスト')]"));
+		WebElement updateLookBack = webDriver.findElement(By.xpath("//td[contains(text(), '来週')]"));
+
+		assertEquals("週報修正テストです。", updateImpression.getText());
+		assertEquals("来週もよろしくお願いします。", updateLookBack.getText());
+
+		// エビデンスの取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
